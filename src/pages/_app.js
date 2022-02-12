2d111c9b8302c04d864ccad2d98fb12fef18080e
nutella <nutell@4ka.com>
2022-02-12 17:24:00 -0500

    feat: add dapp provider and helpers

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,7 +1,12 @@
-import '../styles/globals.css'
+import { DAppProvider } from '@usedapp/core'
+import '@styles/globals.css'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <DAppProvider config={{}}>
+      <Component {...pageProps} />
+    </DAppProvider>
+  )
 }
 
 export default MyApp
