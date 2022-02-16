8e55b3e7d441a39386b33183c2f55d95d4216cc3
nutella <nutell@4ka.com>
2022-02-16 00:33:25 -0500

    feat: update logic for the contract module

--- src/modules/contract/context.js
+++ src/modules/contract/context.js
@@ -1,5 +1,7 @@
 import { createContext, useContext } from "react";
 
-export const ContractsContext = createContext({});
+export const initialContext = { contracts: {}, factories: {} };
+
+export const ContractsContext = createContext(initialContext);
 
 export const useContracts = () => useContext(ContractsContext);
--- src/modules/contract/useContractsApi.js
+++ src/modules/contract/useContractsApi.js
@@ -1,32 +1,67 @@
 import { useReducer } from "react";
-import { getContract } from "@common/utils/contractHelper";
-import { reducer, ADD_CONTRACT_TYPE, REMOVE_CONTRACT_TYPE } from "./reducer";
+import { getContract } from "@common/utils/contractHelpers";
+import {
+  reducer,
+  ADD_CONTRACT_TYPE,
+  REMOVE_CONTRACT_TYPE,
+  ADD_FACTORY_TYPE,
+  REMOVE_FACTORY_TYPE,
+} from "./reducer";
 
 export function useContractsApi(initValue) {
-  const [state, dispatch] = useReducer(reducer, initValue);
+  const [{ contracts, factories }, dispatch] = useReducer(reducer, initValue);
 
-  const addContract = (name, contractAddress, contractAbi) => {
+  const addCreatedContract = (contract) => {
+    if (!contract || !contract?.address)
+      throw new Error("The contract or address don't exist");
+
+    dispatch({
+      type: ADD_CONTRACT_TYPE,
+      payload: { name: contract.address, contract },
+    });
+  };
+
+  const addContract = (contractAddress, contractAbi) => {
     const contract = getContract(contractAddress, contractAbi);
 
-    dispatch({ type: ADD_CONTRACT_TYPE, payload: { name, contract } });
+    dispatch({
+      type: ADD_CONTRACT_TYPE,
+      payload: { name: contractAddress, contract },
+    });
   };
 
   const removeContract = (name) => {
-    if (!state[name]) throw new Error("Contract with name `" + name + "` doesn't exist");
+    if (!state.contracts[name])
+      throw new Error("Contract with name `" + name + "` doesn't exist");
 
     dispatch({ type: REMOVE_CONTRACT_TYPE, payload: { name } });
   };
 
-  const deployContract_ = async (name, contractAbi, contractByteCode) => {
-    const contract = deployContract(contractAbi, contractByteCode);
+  const addFactory = (name, factory) => {
+    if (!name || !factory) throw new Error("Factory data is not full"); // TODO: change error content
 
-    dispatch({ type: DEPLOY_CONTRACT_TYPE, payload: { name, contract } });
+    dispatch({ type: ADD_FACTORY_TYPE, payload: { name, factory } });
+  };
+
+  const addTest = (name, factory) => {
+    dispatch({ type: ADD_FACTORY_TYPE, payload: { name, factory } });
+  };
+
+  const removeFactory = (name) => {
+    if (!state.factories[name])
+      throw new Error("Factory with name `" + name + "` doesn't exist");
+
+    dispatch({ type: REMOVE_FACTORY_TYPE, payload: { name } });
   };
 
   return {
-    contracts: state,
+    contracts,
+    factories,
+    addTest,
+    addCreatedContract,
     addContract,
     removeContract,
-    deployContract: deployContract_,
+    addFactory,
+    removeFactory,
   };
 }
