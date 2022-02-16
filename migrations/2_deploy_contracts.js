ae0c4704a9a42b00d1341d67bec1f0b66e4baa37
Romanelli <romanelli@programist.ru>
2022-02-15 22:28:12 -0500

    fix: remove nft contract from deployment

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,7 +1,5 @@
 const SimpleMarketplaceContract = artifacts.require("SimpleMarketplace");
-const SimpleNFTContract = artifacts.require("SimpleNFT");
 
 module.exports = function (deployer) {
   deployer.deploy(SimpleMarketplaceContract);
-  deployer.deploy(SimpleNFTContract);
 };
