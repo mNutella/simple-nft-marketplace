598f6060c3472575f210260b54176c162ca9458c
nutella <nutell@4ka.com>
2022-02-16 12:21:04 -0500

    feat: add const with config for dapp

---
+++ src/configs/etherConfig.js
@@ -0,0 +1,7 @@
+import { getMulticallAddresses } from "@common/utils/etherHelpers";
+
+export const config = {
+  multicallAddresses: getMulticallAddresses(),
+  autoConnect: true,
+  transactionPath: 'transactions'
+};
