9fdb3df4b8d675df9444c55a403e89451ec42977
nutella <nutell@4ka.com>
2022-02-16 00:44:52 -0500

    feat: implement Input component

---
+++ src/common/components/Input/Input.js
@@ -0,0 +1,30 @@
+import React from "react";
+
+const Input = React.forwardRef(({ id, label, error, ...rest }, ref) => {
+  return (
+    <div className="mb-6">
+      {label && (
+        <label
+          htmlFor={id}
+          className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300"
+        >
+          {label}
+        </label>
+      )}
+      <input
+        className={`bg-gray-50 border text-gray-900 text-sm rounded-lg focus:ring-blue-500 ${
+          error
+            ? "border-red-500 placeholder-red-500"
+            : "border-gray-300 focus:border-blue-500 dark:border-gray-600 dark:placeholder-gray-400"
+        } block w-full p-2.5 dark:bg-gray-700 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500`}
+        id={id}
+        ref={ref}
+        {...rest}
+      />
+    </div>
+  );
+});
+
+Input.displayName = "Input";
+
+export default Input;
