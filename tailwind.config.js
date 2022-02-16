d6ed54a0d4d2bc317d47378deee256c7abbba46c
nutella <nutell@4ka.com>
2022-02-16 12:38:53 -0500

    feat: add flowbite for tailwindcss

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,14 @@
 module.exports = {
+  darkMode: "media",
   content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx}",   
+    "./src/pages/**/*.{js,ts,jsx,tsx}",
     "./src/common/components/**/*.{js,ts,jsx,tsx}",
+    "./node_modules/flowbite/**/*.js",
   ],
   theme: {
     extend: {},
   },
-  plugins: [],
-}
+  plugins: [
+    require("flowbite/plugin"), // TODO: remove flowbite from the project (something strange with WalletInfo bg colors)
+  ],
+};
