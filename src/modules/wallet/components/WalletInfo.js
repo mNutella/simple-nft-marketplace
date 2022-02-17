f5c48b70f854cb3508dff3ff798d44c70dd09127
nutella <nutell@4ka.com>
2022-02-17 16:31:43 -0500

    feat: add dark theme styles for WalletInfo

--- src/modules/wallet/components/WalletInfo.js
+++ src/modules/wallet/components/WalletInfo.js
@@ -11,14 +11,14 @@ export default function WalletInfo({ className }) {
 
   return (
     <div
-      className={`text-white ${className} mx-auto p-2 bg-white rounded-lg border border-gray-200 shadow-md sm:p-6 lg:p-8 dark:bg-gray-800 dark:border-gray-700`}
+      className={`text-white mx-auto p-2 bg-white rounded-lg border border-gray-200 shadow-md sm:p-6 lg:p-8 dark:bg-gray-800 dark:border-gray-700 ${className} `}
     >
       <p className="text-xl text-center font-semibold mb-2">Wallet Info</p>
-      <div className="bg-gray-700 w-min mx-auto p-1 flex whitespace-nowrap rounded-lg">
-        <div className="px-2 py-1 flex-auto mr-1">
+      <div className="dark:bg-gray-700 w-min mx-auto p-1 flex whitespace-nowrap rounded-lg">
+        <div className="px-2 py-1 flex-1 mr-1">
           {balance && <Balance balance={balance} />}
         </div>
-        <div className="px-2 py-1 bg-gray-600 rounded-md">
+        <div className="px-2 py-1 flex-1 text-center dark:bg-gray-600 rounded-lg">
           {account && <Address address={account} />}
         </div>
       </div>
