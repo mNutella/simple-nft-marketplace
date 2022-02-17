edea1a681a59abb941700be01b90c986e6d5e6a2
nutella <nutell@4ka.com>
2022-02-17 15:00:45 -0500

    feat: udpate hook for creating nft

---
+++ src/modules/marketplace/api/useCreateNFT.js
@@ -0,0 +1,47 @@
+import { useEffect, useState } from "react";
+import { utils } from "ethers";
+import { useContractFunction } from "@usedapp/core";
+import { MARKETPLACE_ADDRESS } from "@configs/contractsConfig";
+import { useContracts } from "@modules/contract/context";
+
+export function useCreateNFT() {
+  const { contracts } = useContracts();
+  const contract = contracts[MARKETPLACE_ADDRESS];
+  const {
+    state,
+    send: sendCreateMarketplaceNFT,
+    resetState,
+  } = useContractFunction(contract, "createMarketplaceNFT", {
+    transactionName: "createNFT",
+  });
+  const [inProgress, setInProgress] = useState(false);
+  const [error, setError] = useState(false);
+
+  useEffect(() => {
+    if (state.status === "Success") {
+      setInProgress(false);
+    }
+
+    if (state.status === "Fail" || state.status === "Exception") {
+      setError(true);
+      setInProgress(false);
+    }
+  }, [state]);
+
+  const createNFT = async (name, symbol, url, price) => {
+    setInProgress(true);
+
+    await sendCreateMarketplaceNFT(name, symbol, url, utils.parseEther(price), {
+      value: utils.parseEther("0.025"),
+    });
+  };
+
+  return {
+    inProgress,
+    error,
+    state,
+    createNFT,
+    setError,
+    resetState,
+  };
+}
