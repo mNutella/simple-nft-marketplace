3237e0abffa0acf03cb8d71cba105a1d775f7658
nutella <nutell@4ka.com>
2022-02-17 20:51:11 -0500

    feat: connect ipfs upload to CreateNFTForm

--- src/modules/marketplace/components/CreateNFTForm.js
+++ src/modules/marketplace/components/CreateNFTForm.js
@@ -1,31 +1,47 @@
-import React, { useEffect } from "react";
+import Image from "next/image";
+import React, { useEffect, useRef } from "react";
 import { useEthers } from "@usedapp/core";
 import { Controller, useForm } from "react-hook-form";
 import Button from "@common/components/Button";
 import Input from "@common/components/Input";
 import { useCreateNFT } from "@modules/marketplace/api/useCreateNFT";
+import { useIPFSApi } from "@modules/ipfs/useIPFSApi";
+import { getRandomInt } from "@common/utils/math";
 
 const initFormValues = {
   name: "",
   symbol: "",
   url: "",
   price: 0,
+  file: undefined,
 };
 
 export default function CreateNFTForm() {
   const { account } = useEthers();
   const { inProgress, state, error, setError, createNFT } =
     useCreateNFT(account);
+  const { uploadData } = useIPFSApi();
   const {
     control,
-    formState: { errors },
     handleSubmit,
+    formState: { errors },
     reset,
+    register,
+    setValue,
+    watch,
   } = useForm({
     defaultValues: {
       ...initFormValues,
     },
   });
+  const watchFileUrl = watch("fileUrl", undefined);
+  const imageRef = useRef(null).current;
+
+  useEffect(() => {
+    register("fileUrl");
+  }, [register]);
+
+  useEffect(() => () => URL.revokeObjectURL(watchFileUrl), [watchFileUrl]);
 
   useEffect(() => {
     if (state.status === "Success") {
@@ -36,7 +52,16 @@ export default function CreateNFTForm() {
   if (!account) return null;
 
   const handleFormSubmit = async (data) => {
-    await createNFT(data.name, data.symbol, data.url, data.price);
+    // TODO: add thumbnail
+    // const imageHash = await uploadData(Buffer(data.file.reader?.result));
+    // const metaDataHash = await uploadData(
+    //   JSON.stringify({
+    //     rareness: getRandomInt(100),
+    //     origin: imageHash,
+    //   })
+    // );
+
+    // await createNFT(data.name, data.symbol, metaDataHash, data.price);
   };
 
   return (
@@ -59,6 +84,7 @@ export default function CreateNFTForm() {
           }}
           render={({ field: { onChange, ...rest } }) => (
             <Input
+              id="name"
               label="Name"
               placeholder="SimpleNFT"
               error={errors.name}
@@ -78,6 +104,7 @@ export default function CreateNFTForm() {
           }}
           render={({ field: { onChange, ...rest } }) => (
             <Input
+              id="symbol"
               label="Symbol"
               placeholder="MFT"
               error={errors.symbol}
@@ -90,6 +117,39 @@ export default function CreateNFTForm() {
           )}
         />
         <Controller
+          name="file"
+          control={control}
+          rules={{
+            required: { value: true, message: "This is required input" },
+          }}
+          render={({ field: { onChange, ...rest } }) => (
+            <Input
+              id="file"
+              type="file"
+              label="File"
+              accept="image/*"
+              helper="File types supported: JPG, PNG, GIF, SVG. Max size: 100 MB"
+              error={errors.file}
+              onChange={(e) => {
+                setError(false);
+                setValue("fileUrl", URL.createObjectURL(e.fileData));
+                onChange(e);
+              }}
+              {...rest}
+            />
+          )}
+        />
+        {watchFileUrl && (
+          <Image
+            className="rounded-lg max-w-10 mb-6 object-cover"
+            width={100}
+            height={100}
+            ref={imageRef}
+            src={watchFileUrl}
+            alt="preview image"
+          />
+        )}
+        {/* <Controller
           name="url"
           control={control}
           rules={{
@@ -108,7 +168,7 @@ export default function CreateNFTForm() {
               {...rest}
             />
           )}
-        />
+        /> */}
         <Controller
           name="price"
           control={control}
@@ -118,9 +178,10 @@ export default function CreateNFTForm() {
           }}
           render={({ field: { onChange, ...rest } }) => (
             <Input
+              id="price"
               type="number"
               label="Price"
-              placeholder="MFT"
+              placeholder="0.007"
               error={errors.price}
               onChange={(e) => {
                 setError(false);
