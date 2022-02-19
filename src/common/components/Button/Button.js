47c4589dcaa32e0831e0256100c41c2ff9d9b434
nutella <nutell@4ka.com>
2022-02-19 03:18:37 -0500

    styles: update Button

--- src/common/components/Button/Button.js
+++ src/common/components/Button/Button.js
@@ -2,18 +2,21 @@ import React from "react";
 import Spinner from "../SVGs/Spinner";
 
 export default function Button({
-  text,
-  icon: Icon,
   type = "button",
+  className,
   loading,
   disabled,
+  children,
   onClick,
 }) {
   return (
     <button
       type={type}
       disabled={disabled || loading}
-      className="text-gray-900 bg-white hover:bg-gray-100 border border-gray-200 focus:ring-4 focus:ring-gray-100 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:focus:ring-gray-600 dark:bg-gray-800 dark:border-gray-700 dark:text-white dark:hover:bg-gray-700 mr-2 mb-2"
+      className={
+        "font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center mr-2 mb-2 bg-gradient-to-r text-white dark:text-white from-blue-500 via-blue-600 to-blue-700 hover:bg-gradient-to-br focus:ring-4 focus:ring-blue-300 dark:focus:ring-blue-800 shadow-lg shadow-blue-500/50 dark:shadow-lg dark:shadow-blue-800/80 " +
+        className
+      }
       onClick={onClick}
     >
       {loading ? (
@@ -22,10 +25,7 @@ export default function Button({
           Loading...
         </>
       ) : (
-        <>
-          {Icon}
-          {text}
-        </>
+        children
       )}
     </button>
   );
