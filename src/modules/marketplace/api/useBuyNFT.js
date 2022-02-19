d060558fdd849d7710a2466a14cbd1fc24fcd163
nutella <nutell@4ka.com>
2022-02-19 03:40:02 -0500

    feat: implement hook to buy nft

---
+++ src/modules/marketplace/api/useBuyNFT.js
@@ -0,0 +1,33 @@
+import { useContractFunction } from "@usedapp/core";
+import { formatEther, parseEther } from "@ethersproject/units";
+import useTransactionStatuses from "@common/hooks/useTransactionStatuses";
+import { useContracts } from "@modules/contract/context";
+import { MARKETPLACE_ADDRESS } from "@configs/contractsConfig";
+
+export function useBuyNFT() {
+  const { contracts } = useContracts();
+  const contract = contracts[MARKETPLACE_ADDRESS];
+  const {
+    state,
+    send: sendCreateMarketplaceSale,
+    resetState,
+  } = useContractFunction(contract, "createMarketplaceSale", {
+    transactionName: "saleNFT",
+  });
+  const { inProgress, setInProgress, error } = useTransactionStatuses(state);
+
+  const buyNFT = async (nftAddress, price) => {
+    setInProgress(true);
+    await sendCreateMarketplaceSale(nftAddress, 1, {
+      value: parseEther(formatEther(price)),
+    });
+  };
+
+  return {
+    inProgress,
+    error,
+    state,
+    buyNFT,
+    resetState,
+  };
+}
--- src/modules/marketplace/components/Explore.js
+++ src/modules/marketplace/components/Explore.js
@@ -2,16 +2,22 @@ import React from "react";
 import { useEthers } from "@usedapp/core";
 import GridList from "@common/components/GridList";
 import { useGetItems } from "../api/useGetItems";
+import { useBuyNFT } from "../api/useBuyNFT";
 
 export default function Explore() {
   const { account } = useEthers();
   const { data } = useGetItems();
+  const { buyNFT } = useBuyNFT();
 
   if (!account) return null;
 
+  const handleBuy = async ({ contractAddress, price }) => {
+    await buyNFT(contractAddress, price);
+  }
+
   return (
-    <div className="px-2">
-      <GridList items={data} />
+    <div>
+      <GridList items={data} currentAddress={account} onBuy={handleBuy} />
     </div>
   );
 }
