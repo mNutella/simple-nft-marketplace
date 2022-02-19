a7f165a2705cbf3ae171a552ab55af9f9acc983a
nutella <nutell@4ka.com>
2022-02-19 03:36:09 -0500

    chore: update configs and helpers

--- src/configs/etherConfig.js
+++ src/configs/etherConfig.js
@@ -1,7 +1,9 @@
 import { getMulticallAddresses } from "@common/utils/etherHelpers";
+import { Localhost, Mainnet } from "@usedapp/core";
 
 export const config = {
   multicallAddresses: getMulticallAddresses(),
   autoConnect: true,
+  networks: [Localhost, Mainnet],
   transactionPath: 'transactions'
 };
