d996152922b820b6b45512cd29d304a90708bb18
nutella <nutell@4ka.com>
2022-02-19 03:23:43 -0500

    feat: add buy button to GridListItem and add stub for empty GridList

--- src/common/components/GridList/GridList.js
+++ src/common/components/GridList/GridList.js
@@ -1,18 +1,27 @@
-import { formatEther } from "@ethersproject/units";
 import React from "react";
 import GridListItem from "./GridListItem";
 
-export default function GridList({ items }) {
-  if (!items || !items.length) return null;
+export default function GridList({ currentAddress, items, onBuy }) {
+  if (!items || !items.length)
+    return (
+      <h1 className="text-2xl mb-2 dark:text-gray-600 font-light text-center">
+        Empty List
+      </h1>
+    );
 
   return (
     <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-6 gap-4 mb-2">
       {items.map((item) => (
         <GridListItem
           key={item[0].toString()}
+          currentAddress={currentAddress}
+          sellerAddress={item[3]}
+          contractAddress={item[1]}
           metaInfoHash={item[7]}
-          tokenId={item[0].toString()}
-          price={parseFloat(formatEther(item[5])).toFixed(4)}
+          itemId={item[0]}
+          tokenId={item[2]}
+          price={item[5]}
+          onBuy={onBuy}
         />
       ))}
     </div>
--- src/common/components/GridList/GridListItem.js
+++ src/common/components/GridList/GridListItem.js
@@ -1,9 +1,21 @@
 import React, { useEffect, useState } from "react";
 import Image from "next/image";
-import PolygonIcon from "../SVGs/PolygonIcon";
+import { formatEther } from "@ethersproject/units";
+import PolygonIcon from "@common/components/SVGs/PolygonIcon";
+import Button from "@common/components/Button";
 import { getIPFSFileUrl } from "@common/utils/ipfsHelpers";
+import { isSameAccount } from "@common/utils/etherHelpers";
 
-export default function GridListItem({ metaInfoHash, tokenId, price }) {
+export default function GridListItem({
+  metaInfoHash,
+  currentAddress,
+  sellerAddress,
+  itemId,
+  tokenId,
+  price,
+  contractAddress,
+  onBuy,
+}) {
   const [name, setName] = useState("");
   const [thumbnailUrl, setThumbnailUrl] = useState("");
   const [error, setError] = useState(false);
@@ -27,8 +39,12 @@ export default function GridListItem({ metaInfoHash, tokenId, price }) {
 
   if (loading || error) return null;
 
+  const handleBuyClick = () => {
+    onBuy && onBuy({ contractAddress, itemId, price });
+  };
+
   return (
-    <div className="w-full bg-white rounded-lg border border-gray-200 shadow-md dark:bg-gray-800 dark:border-gray-700 transition duration-300 ease-in-out hover:scale-105">
+    <div className="w-full bg-white rounded-lg border border-gray-200 shadow-md cursor-pointer dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-900 group transition duration-300 ease-in-out hover:scale-105">
       <>
         {thumbnailUrl && (
           <Image
@@ -37,22 +53,27 @@ export default function GridListItem({ metaInfoHash, tokenId, price }) {
             width="100%"
             height="100%"
             layout="responsive"
-            objectFit="contain"
+            objectFit="cover"
             alt={name + " image"}
           />
         )}
         <div className="p-5">
-          <a href="#">
-            <h5 className="mb-2 text-xl font-bold tracking-tight text-gray-900 dark:text-white">
-              {name} #{tokenId}
-            </h5>
-          </a>
-          <div className="inline-flex items-center py-2 px-3 text-sm font-medium text-center text-white bg-blue-700 rounded-lg dark:bg-blue-600">
-            {price}
-            <span className="ml-2 w-3">
-              <PolygonIcon />
+          <h5 className="mb-2 text-xl font-bold tracking-tight text-gray-900 dark:text-white">
+            {name} #{itemId.toString()}
+          </h5>
+          <div className="inline-flex items-center py-1 px-2 text-sm font-medium text-center text-white bg-gray-300 rounded-lg dark:bg-gray-700 transition duration-300 group-hover:bg-gray-800 mb-2">
+            {formatEther(price)}
+            <span className="ml-2 w-4">
+              <PolygonIcon className="fill-blue-400" />
             </span>
           </div>
+          {!isSameAccount(currentAddress, sellerAddress) && (
+            <div className="w-full">
+              <Button className="w-full text-center" onClick={handleBuyClick}>
+                <span className="w-full">Buy</span>
+              </Button>
+            </div>
+          )}
         </div>
       </>
     </div>
