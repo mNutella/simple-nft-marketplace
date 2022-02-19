d7164d5f3c289c3192bb661501299714e6f6e836
nutella <nutell@4ka.com>
2022-02-19 03:35:04 -0500

    feat: extract development mode check to helper and add check of two accounts

--- src/common/utils/etherHelpers.js
+++ src/common/utils/etherHelpers.js
@@ -1,9 +1,16 @@
 import { Mainnet, Localhost } from '@usedapp/core'
+import { isDevelopment } from './coreHelpers';
 
 export function getMulticallAddresses() {
-  if (process.env.NODE_ENV === "development") {
+  if (isDevelopment()) {
     return { [Localhost.chainId]: process.env.NEXT_PUBLIC_MULTICALL_CONTRACT_ADDRESS }
   }
 
   return { [Mainnet.chainId]: process.env.NEXT_PUBLIC_MULTICALL_CONTRACT_ADDRESS };
+}
+
+export function isSameAccount(accountA, accountB) {
+  if (!accountA || !accountB) return false;
+
+  return accountA.toLowerCase() === accountB.toLowerCase();
 }
