fe1e7c588464b6bc4712cf7684bbf6bec38aaa40
nutella <nutell@4ka.com>
2022-02-21 14:36:47 -0500

    chore: remove SignOut btn from MetaMaskAuthForm

--- src/modules/metamask-auth/components/MetaMaskAuthForm.js
+++ src/modules/metamask-auth/components/MetaMaskAuthForm.js
@@ -4,17 +4,21 @@ import Button from "@common/components/Button";
 import MetaMaskIcon from "@common/components/SVGs/MetaMaskIcon";
 
 export default function MetaMaskAuthForm() {
-  const { account, activateBrowserWallet, deactivate } = useEthers();
+  const { account, activateBrowserWallet } = useEthers();
+
+  if (account) return null;
 
   return (
     <div>
       {!account && (
-        <Button onClick={activateBrowserWallet}>
+        <Button
+          appendClassName="inline-flex items-center"
+          onClick={activateBrowserWallet}
+        >
           <MetaMaskIcon />
           <span className="ml-1">Connect with MetaMask</span>
         </Button>
       )}
-      {account && <Button onClick={deactivate}>Sign Out</Button>}
     </div>
   );
 }
