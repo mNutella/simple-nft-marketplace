81f9cccf880bcdd9641a981fdfb1f05a1ad8bb15
nutella <nutell@4ka.com>
2022-02-21 19:56:59 -0500

    chore: add new multical addresses to config and little fixes

--- src/common/utils/etherHelpers.js
+++ src/common/utils/etherHelpers.js
@@ -1,12 +1,28 @@
-import { Mainnet, Localhost } from '@usedapp/core'
-import { isDevelopment } from './coreHelpers';
+import { ChainId } from "@usedapp/core";
+import { isDevelopment } from "./coreHelpers";
 
 export function getMulticallAddresses() {
   if (isDevelopment()) {
-    return { [Localhost.chainId]: process.env.NEXT_PUBLIC_MULTICALL_CONTRACT_ADDRESS }
+    return {
+      [ChainId.Localhost]: process.env.NEXT_PUBLIC_MULTICALL_CONTRACT_ADDRESS,
+    };
   }
 
-  return { [Mainnet.chainId]: process.env.NEXT_PUBLIC_MULTICALL_CONTRACT_ADDRESS };
+  if (ChainId.Mumbai === process.env.NEXT_PUBLIC_NETWORK) {
+    return {
+      [ChainId.Mumbai]: process.env.NEXT_PUBLIC_MULTICALL_CONTRACT_ADDRESS,
+    };
+  }
+
+  if (ChainId.Polygon === process.env.NEXT_PUBLIC_NETWORK) {
+    return {
+      [ChainId.Polygon]: process.env.NEXT_PUBLIC_MULTICALL_CONTRACT_ADDRESS,
+    };
+  }
+
+  return {
+    [ChainId.Mainnet]: process.env.NEXT_PUBLIC_MULTICALL_CONTRACT_ADDRESS,
+  };
 }
 
 export function isSameAccount(accountA, accountB) {
--- src/configs/etherConfig.js
+++ src/configs/etherConfig.js
@@ -1,9 +1,7 @@
 import { getMulticallAddresses } from "@common/utils/etherHelpers";
-import { Localhost, Mainnet } from "@usedapp/core";
 
 export const config = {
   multicallAddresses: getMulticallAddresses(),
   autoConnect: true,
-  networks: [Localhost, Mainnet],
-  transactionPath: 'transactions'
+  transactionPath: "transactions",
 };
