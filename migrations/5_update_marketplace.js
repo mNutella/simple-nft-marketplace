711377f91d4c231f773afd26a1f8c61e9b48de36
Romanelli <romanelli@programist.ru>
2022-05-05 04:29:42 -0400

    feat: add migrations for marketplace

---
+++ migrations/5_update_marketplace.js
@@ -0,0 +1,8 @@
+const SimpleMarketplaceContract = artifacts.require("SimpleMarketplace");
+
+module.exports = function (deployer) {
+  deployer.deploy(
+    SimpleMarketplaceContract,
+    "0x8aeE3cdeD117De523bDE77B8de71EdcDFc1cf3EF"
+  );
+};
